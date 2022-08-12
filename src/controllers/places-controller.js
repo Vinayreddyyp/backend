924e0ac813765f18504ee8b0757bde9b86739b49
vinayreddyyp <ypvinayreddy@gmail.com>
2022-08-12 12:01:43 -0700

    add place controller to the app

--- src/controllers/places-controller.js
+++ src/controllers/places-controller.js
@@ -105,8 +105,9 @@ const createPlace = async (req, res, next) => {
 	res.status(201).json({ place: createdPlace });
 };
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
 	const errors = validationResult(req);
+
 	if (!errors.isEmpty()) {
 		throw new HttpError("Invalid inputs passes,please check your data ", 422);
 	}
@@ -114,12 +115,24 @@ const updatePlace = (req, res, next) => {
 
 	const placeId = req.params.pid;
 
-	const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
-	const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
-	updatedPlace.title = title;
-	updatedPlace.description = description;
-	DUMMY_PLACES[placeIndex] = updatePlace;
-	res.status(200).json({ place: updatedPlace });
+	let place;
+	try {
+		place = await Place.findById(placeId);
+	} catch (err) {
+		const error = new HttpError("something went wrong", 500);
+		return next(error);
+	}
+	place.title = title;
+	place.description = description;
+
+	try {
+		await place.save();
+	} catch (err) {
+		const error = new HttpError("something went wrong", 500);
+		return next(error);
+	}
+
+	res.status(200).json({ place: place.toObject({ getters: true }) });
 };
 
 const deletePlace = (req, res) => {
