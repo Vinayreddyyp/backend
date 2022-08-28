e89cab3e973b7cd556fa41be99b72cbeaa410699
vinayreddyyp <ypvinayreddy@gmail.com>
2022-08-27 18:48:03 -0700

    add cross compatability to the app

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+	res.setHeader("Access-control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-with, Content-Type,Accept, Authorization"
+	);
+	res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH, DELETE");
+	next();
+});
+
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
