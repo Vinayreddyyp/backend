ec267290bdb96e1b63141f71c3db814f6dabcc5d
vinayreddyyp <ypvinayreddy@gmail.com>
2022-08-27 18:49:11 -0700

    add location to the app

--- src/util/location.js
+++ src/util/location.js
@@ -1,7 +1,14 @@
 const axios = require("axios");
 const HttpError = require("../models/http-error");
+const API_KEY = "AIzaSyDt7HEo0TWKd8Ox97QCXKPSBAe4gXVeqe8";
 
 async function getCoordsForAddress(address) {
+	const response = await axios.get(
+		`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+			address
+		)}&key=${API_KEY}`
+	);
+
 	const data = response.data;
 
 	if (!data || data.status === "ZERO_RESULTS") {
