2261830c0e917a19e8ea0f657a7d6fde08d3ce1f
vinayreddyyp <ypvinayreddy@gmail.com>
2022-09-30 21:02:12 -0700

    add check authorization

--- src/middleware/check-auth.js
+++ src/middleware/check-auth.js
@@ -1,10 +1,16 @@
 const HttpError = require("../models/http-error");
+const jwt = require("jsonwebtoken");
+
 module.exports = (req, res, next) => {
 	try {
 		const token = req.headers.authorization.split(" ")[1];
 		if (!token) {
 			throw Error("Invalid authorization");
 		}
+
+		const decodedToken = jwt.verify(token, "supersecret_do_not_share");
+		req.userData = { userId: decodedToken.userId };
+		next();
 	} catch (err) {
 		const error = new HttpError("Invalid authorization", 401);
 		return next(error);
